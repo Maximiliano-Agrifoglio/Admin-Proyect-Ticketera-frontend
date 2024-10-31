9dc3307f7e51fd41bb690b0819309a3480c8610b
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2024-10-31 19:28:05 -0300

    creando el routing

---
+++ admin-proyect-vite-frontend/src/Router.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardView from "./views/DashboardView";
+import AppLayout from "./layouts/AppLayout";
+
+const Router = () => {
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />}>
+                   <Route path="/" element={<DashboardView />} index />
+                </Route>
+                
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default Router;
+
