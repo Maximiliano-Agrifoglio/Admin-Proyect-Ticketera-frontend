8778da431040285b0f5998509f9d3536f75db582
Maximiliano-Agrifoglio <maxiagrifoglio966@gmail.com>
2024-11-01 18:42:55 -0300

    URL-Base

--- admin-proyect-vite-frontend/src/Router.tsx
+++ admin-proyect-vite-frontend/src/Router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardView from "./views/DashboardView";
-import AppLayout from "./layouts/AppLayout";
+import DashboardView from "@/views/DashboardView";
+import AppLayout from "@/layouts/AppLayout";
 
 const Router = () => {
   return (
